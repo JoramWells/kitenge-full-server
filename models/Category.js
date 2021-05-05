2c720471b713bcecd48780ffc79eabc718f10d87
unknown <jorammanoah1@gmail.com>
2021-05-05 02:09:33 -0700

    Populated category

--- models/Category.js
+++ models/Category.js
@@ -1,2 +1,20 @@
-const Sequelize = require('sequelize')
-const db = require('../config/connection')
+const Sequelize = require("sequelize");
+const db = require("../config/connection");
+
+//create Category model
+const Category = db.define("category", {
+  category: {
+    type: Sequelize.STRING,
+  },
+  createdAt:{
+    type:Sequelize.DATE,
+  },
+  updatedAt:{
+    type:Sequelize.DATE,
+  },
+});
+
+Category.beforeSync(() => console.log("Creating category table.."));
+Category.afterSync(() => console.log("Created category table!"));
+
+module.exports = Category;
