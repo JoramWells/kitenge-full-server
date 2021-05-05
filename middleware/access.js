3ee3176f2d5ca8c9b5db3391da084f23be458058
unknown <jorammanoah1@gmail.com>
2021-05-05 02:49:12 -0700

    Modified acces

--- middleware/access.js
+++ middleware/access.js
@@ -1,12 +1,11 @@
 const axios = require("axios");
+require('dotenv').config()
 
-const consumer_key = "GFLaeINfznCZgALQ2IlDjrQ8rqtUkfll";
-const consumer_secret = "X5ejdUNKIAUeHN6m";
 const url =
   "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
 
 const access = async (req, res, next) => {
-  let buff = Buffer.from(consumer_key + ":" + consumer_secret);
+  let buff = Buffer.from(process.env.CONSUMER_KEY + ":" + process.env.CONSUMER_SECRET);
   let auth = buff.toString("base64");
   const { data } = await axios.get(url, {
     headers: {
