1ec3ec4212f52f996c86eda09fd8f8f93fd839c7
unknown <jorammanoah1@gmail.com>
2021-05-05 21:33:40 -0700

    Added async

--- routes/order.js
+++ routes/order.js
@@ -4,8 +4,8 @@ const Order = require("../models/Order");
 const router = express.Router();
 
 //order routes
-router.get("/orders", (req, res) => {
-  Order.findAll({})
+router.get("/orders",async (req, res) => {
+  await Order.findAll({})
     .then((items) => res.send(items))
     .catch((err) => console.log(err));
 });
